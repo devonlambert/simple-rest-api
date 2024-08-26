94da9d6f26e4d43cf46a0a143c63aa079bf0da8f
Devon Lambert <devon@realityknights.com>
2024-08-25 20:21:38 -0500

    Adds Blueprints for Stores and Items

--- app.py
+++ app.py
@@ -1,57 +1,21 @@
-import uuid
-from flask import Flask, request
-from flask_smorest import abort
-from db import items, stores
+from flask import Flask
+from flask_smorest import Api
+
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
+
 
 app = Flask(__name__)
 
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["API_TITLE"] = "Stores REST API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist"
 
-# GET Get all stores
-@app.get("/store")
-def get_stores():
-    return { "stores": list(stores.values()) }
+api = Api(app)
 
-# GET An indidivual Store
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
-    try: 
-        return stores[store_id]
-    except KeyError:
-        abort(404, message="Store not found.")
-    
-# POST Create a store
-@app.post("/store")
-def create_store():
-    store_data = request.get_json()
-    store_id = uuid.uuid4().hex
-    
-    store = {**store_data, "id": store_id}
-    stores[store_id] = new_store
-    
-    return store, 201
-
-# POST Create an item
-@app.post("/item")
-def create_item():
-    item_data = request.get_json()
-    if item_data["store_id"] not in stores:
-        abort(404, message="Store not found.")
-        
-    item_id = uuid.uuid4().hex
-    item = {**item_data, "id": item_id}
-    items[item_id] = item
-    
-    return item, 201
-
-# GET all items
-@app.get("/item")
-def get_items():
-    return { "items": list(items.values()) }
-
-# GET An individual item
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id]
-    except KeyError:
-        abort(404, message="Item not found.")
+api.register_blueprint(ItemBlueprint)
+api.register_blueprint(StoreBlueprint)
---
+++ resources/store.py
@@ -0,0 +1,50 @@
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from db import stores
+
+blp = Blueprint("stores", __name__, description="Operations on stores")
+
+@blp.route("/store/<string:store_id>")
+class Store(MethodView):
+    # Get a store
+    def get(self, store_id):
+        try: 
+            return stores[store_id]
+        except KeyError:
+            abort(404, message="Store not found.")
+    
+    # Delete a store
+    def delete(self, store_id):
+        try:
+            del stores[store_id]
+            return {"message": "Store deleted."}
+        except KeyError:
+            abort(404, message="Store not found.")
+            
+
+@blp.route("/store")
+class StoreList(MethodView):
+    
+    # Get all stores
+    def get(self):
+        return { "stores": list(stores.values()) }
+    
+    # Create a store
+    def post(self):
+        store_data = request.get_json()
+        if "name" not in store_data:
+            abort(
+                400,
+                message="Bad request. Ensure 'name' is included in JSON payload.",
+            )
+        for store in stores.values():
+            if store_data["name"] == store["name"]:
+                abort(400, message=f"Store already exists.")
+        
+        store_id = uuid.uuid4().hex
+        store = {**store_data, "id": store_id}
+        stores[store_id] = store
+        
+        return store
