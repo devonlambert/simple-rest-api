c2ce77ea239132f04acfbfc2b73fecbc98edb66f
Devon Lambert <devon@realityknights.com>
2024-08-25 21:31:35 -0500

    Adds Marshmallow Schemas and Fixes Swagger-UI Docs

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.config["API_VERSION"] = "v1"
 app.config["OPENAPI_VERSION"] = "3.0.3"
 app.config["OPENAPI_URL_PREFIX"] = "/"
 app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.17.14/"
 
 api = Api(app)
 
